4d946cfe6e7cca0de4a884df813842771c17f335
Kevin <slash2130kevin@gmail.com>
2022-10-23 21:17:46 -0500

    Calculadora terminada (Diseño a medias)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora-app';
+  titulo : string = 'Aplicación Calculadora';
+  operandoA : number = 0;
+  operandoB : number = 0;
+  resultado : number =  0;
+
+  public sumar() : void {
+    this.resultado = this.operandoA + this.operandoB;
+  }
+
+  public restar() : void {
+    this.resultado = this.operandoA - this.operandoB;
+  }
+
+  public multiplicar() : void {
+    this.resultado = this.operandoA * this.operandoB
+  }
+
+  public dividir() : void {
+
+    if (this.operandoB == 0) {
+      this.resultado =  0;
+      return;
+    }
+    
+    this.resultado = this.operandoA / this.operandoB;
+  }
+
 }
