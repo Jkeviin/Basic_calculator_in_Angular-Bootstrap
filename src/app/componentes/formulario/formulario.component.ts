42d75d700f50e0982248e927b409788e2fbb4b45
Kevin <slash2130kevin@gmail.com>
2022-10-24 14:23:31 -0500

    Validacion de operacion Agregado y Agregado Boton limpiar

--- src/app/componentes/formulario/formulario.component.ts
+++ src/app/componentes/formulario/formulario.component.ts
@@ -13,18 +13,23 @@ export class FormularioComponent{
   operandoB : number = 0;
 
   public sumar() : void {
+    if(!this.Validacion()) return;
     this.resultadoCreado.emit(this.operandoA + this.operandoB);
   }
 
   public restar() : void {
+    if(!this.Validacion()) return;
     this.resultadoCreado.emit(this.operandoA - this.operandoB);
   }
 
   public multiplicar() : void {
+    if(!this.Validacion()) return;
     this.resultadoCreado.emit(this.operandoA * this.operandoB);
   }
 
   public dividir() : void {
+
+    if(!this.Validacion()) return;
 
     if (this.operandoB == 0) {
       this.resultadoCreado.emit(0);
@@ -33,4 +38,18 @@ export class FormularioComponent{
     this.resultadoCreado.emit(this.operandoA / this.operandoB);
   }
 
+  private Validacion(): boolean {
+    if(this.operandoA == null || this.operandoB == null){
+      alert("Los operandos no pueden ser nulos");
+      return false
+    }
+
+    return true
+  }
+
+  public limpiar() : void {
+    this.operandoA = 0;
+    this.operandoB = 0;
+    this.resultadoCreado.emit(0);
+  }
 }
